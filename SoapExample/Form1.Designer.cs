9cf998bf97ed29f26e92c2a65467a0ef07ffd76d
akwacz <23086908+akwacz@users.noreply.github.com>
2018-03-01 12:31:33 +0100

    Add files via upload

--- SoapExample/Form1.Designer.cs
+++ SoapExample/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.apiAddress = new System.Windows.Forms.TextBox();
+            this.requestText = new System.Windows.Forms.RichTextBox();
+            this.responseText = new System.Windows.Forms.RichTextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.responseText, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.apiAddress, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.requestText, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.sendButton, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1004, 475);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "API Interface address";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // apiAddress
+            // 
+            this.apiAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.apiAddress, 3);
+            this.apiAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.apiAddress.Location = new System.Drawing.Point(254, 4);
+            this.apiAddress.Name = "apiAddress";
+            this.apiAddress.Size = new System.Drawing.Size(747, 22);
+            this.apiAddress.TabIndex = 1;
+            // 
+            // requestText
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.requestText, 2);
+            this.requestText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.requestText.Location = new System.Drawing.Point(3, 33);
+            this.requestText.Name = "requestText";
+            this.requestText.Size = new System.Drawing.Size(496, 409);
+            this.requestText.TabIndex = 2;
+            this.requestText.Text = "";
+            // 
+            // responseText
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.responseText, 2);
+            this.responseText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.responseText.Location = new System.Drawing.Point(505, 33);
+            this.responseText.Name = "responseText";
+            this.responseText.Size = new System.Drawing.Size(496, 409);
+            this.responseText.TabIndex = 3;
+            this.responseText.Text = "";
+            // 
+            // sendButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.sendButton, 4);
+            this.sendButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendButton.Location = new System.Drawing.Point(3, 448);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(998, 24);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "SendRequest";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1004, 475);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "SoapExample";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox responseText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox apiAddress;
+        private System.Windows.Forms.RichTextBox requestText;
+        private System.Windows.Forms.Button sendButton;
     }
 }
 
--- SoapExample/Form1.cs
+++ SoapExample/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SoapExample
 {
@@ -16,5 +12,44 @@ namespace SoapExample
         {
             InitializeComponent();
         }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if(apiAddress.Text.Length > 5 && requestText.Text.Length > 5)
+                Execute(requestText.Text.ToString(), apiAddress.Text.ToString(), responseText);
+        }
+
+
+        public static void Execute(string req, string api, RichTextBox resultTextbox)
+        {
+            HttpWebRequest request = CreateWebRequest(api);
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml(req);
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    string soapResult = rd.ReadToEnd();
+                    resultTextbox.Text = soapResult;
+                }
+            }
+        }
+
+        public static HttpWebRequest CreateWebRequest(string apiAddress)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(apiAddress);
+            webRequest.Headers.Add(@"SOAP:Action");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
     }
 }
